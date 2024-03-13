bf562f98c6cea05377c0c7168792e2f27466b0c1
Guaxez <vricardimelo@gmail.com>
2024-03-12 22:00:36 -0300

    redux initial implementation
    created store, root reducer and user reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
@@ -8,20 +10,23 @@ import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
+import { store } from './store/store';
 
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+        <UserProvider>
+            <CategoriesProvider>
+            <CartProvider>
+                <App />
+            </CartProvider>
+            </CategoriesProvider>
+        </UserProvider>
+        </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
