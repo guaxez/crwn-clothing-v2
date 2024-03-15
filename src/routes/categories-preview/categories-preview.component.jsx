0127cae9804b5145a3fed82f99ba377397016910
vinicius.melo <vinicius.melo@db1.com.br>
2024-03-15 10:38:13 -0300

    categories selector

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,13 +1,15 @@
-import { useContext, Fragment } from 'react';
+import { Fragment } from 'react';
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { useSelector } from 'react-redux';
+
 import CategoryPreview from '../../components/category-preview/category-preview.component';
+import { selectCategoriesMap } from '../../store/categories/category.select';
 
 const CategoriesPreview = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
 
   return (
-    <Fragment>
+    <Fragment> 
       {Object.keys(categoriesMap).map((title) => {
         const products = categoriesMap[title];
         return (
