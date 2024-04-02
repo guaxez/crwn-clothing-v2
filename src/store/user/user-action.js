54730835af60f3df74fa6904f7cd32147e509a4c
vinicius.melo <vinicius.melo@db1.com.br>
2024-04-02 11:38:31 -0300

    user saga final implementation

--- src/store/user/user-action.js
+++ src/store/user/user-action.js
@@ -24,3 +24,28 @@ export const signInSuccess = (user) => {
 export const signInFailed = (error) => {
     return createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error)
 }
+
+export const signUpStart = (email, password, displayName) => {
+    return createAction(USER_ACTION_TYPES.SIGN_UP_START, { email, password, displayName })
+}
+
+export const signUpSuccess = (user, additionalDetails) => {
+    return createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails })
+}
+
+export const signUpFailed = (error) => {
+    return createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error);
+}
+
+export const signOutStart = () => {
+    return createAction(USER_ACTION_TYPES.SIGN_OUT_START);
+}
+
+export const signOutSuccess = () => {
+    return createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS);
+}
+
+export const signOutFailed = (error) => {
+    return createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error);
+}
+
